a2188565976d4fee5551d106f9215f1930c2db25
Maricruz Valdez <isc_valdez.m@tesvb.edu.mx>
2018-06-04 17:42:44 -0500

    error

--- index.php
+++ index.php
@@ -19,5 +19,5 @@
     Views\Template::header();
     AppData\Config\Enrutador::run(new AppData\Config\Request());
     if(!isset($_SERVER["HTTP_X_REQUESTED_WITH"]))
-    Views\Template::footer();
+
 ?>
